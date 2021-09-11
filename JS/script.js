f9d511642ed632b316e47ccd4b6b11cc2ce607a9
mwendaB <brian.mwenda@moringaschool.com>
2021-09-11 21:10:59 +0300

    enter the popup effects succesfuly

--- JS/script.js
+++ JS/script.js
@@ -7,20 +7,23 @@ $(document).ready(function () {
       });
     });
 
-    // $(".img2").click(function(){
-    //    $(".img2").toggle(function() {
-    //        $("h32").css();
-    //    });
-    // });
-
-    $(".img1").click(function () {
-        $(".img1").toggle(function () {
-          $(".paragraph1").show(function () {
-            $("#h31").css('font-weight', 'bold');
+    
+    $(".img2").click(function () {
+        $(".img2").toggle(function () {
+          $(".paragraph2").show(function () {
+            $("#h32").css('font-weight', 'bold');
           });
         });
       });
+     
 
+      $(".img3").click(function () {
+        $(".img3").toggle(function () {
+          $(".paragraph3").show(function () {
+            $("#h33").css('font-weight', 'bold');
+          });
+        });
+      });
 
 
 });
