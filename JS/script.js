c17dff680a049bfcbd6118444a4809e39586495e
mwendaB <brian.mwenda@moringaschool.com>
2021-09-12 20:09:53 +0300

    add google fonts link

--- JS/script.js
+++ JS/script.js
@@ -7,7 +7,17 @@ $(document).ready(function () {
       });
     });
 
-    
+
+    $(".paragraph1").click(function (){
+      $(".paragraph1").hide(function () {
+        $(".img1").show(function () {
+          $("#header1").css('font-size','normal', 'font-weight','bold');
+        });
+      });
+    });
+  })
+      
+  $(document).ready(function () {
     $(".img2").click(function () {
         $(".img2").toggle(function () {
           $(".paragraph2").show(function () {
@@ -15,8 +25,18 @@ $(document).ready(function () {
           });
         });
       });
+
+      $(".paragraph2").click(function () {
+        $(".paragraph2").toggle(function () {
+          $(".img2").show(function () {
+            $("#header2").css('font-size','normal', 'font-weight','bold');
+          });
+        });
+      });
+    });    
      
 
+    $(document).ready(function () {
       $(".img3").click(function () {
         $(".img3").toggle(function () {
           $(".paragraph3").show(function () {
@@ -25,35 +45,13 @@ $(document).ready(function () {
         });
       });
 
-
-
-  $(".paragraph1").click(function (){
-    $(".paragraph1").toggle(function () {
-      $(".img1").show(function () {
-        $("#header1").css('font-size','normal', 'font-weight','bold');
-      });
-    });
-  });
-
-
-  $(".paragraph2").click(function () {
-    $(".paragraph2").toggle(function () {
-      $(".img2").show(function () {
-        $("#header2").css('font-size','normal', 'font-weight','bold');
-      });
-    });
-  });
-
   $(".paragraph3").click(function () {
     $(".paragraph3").toggle(function () {
       $(".img3").show(function () {
         $("#header3").css('font-size','normal', 'font-weight','bold');
       });
     });
-  });
-
-
-  
+  }); 
 });
 
 $(document).ready(function(){
